b7a702f9f1b4e8805affbb0bc913a109434c8d05
robin <engel.robin@gmx.de>
2016-12-07 21:18:10 +0100

    initial commit

---
+++ src/main/java/org/bluedevel/hollidaymanager/HollidaymanagerApplication.java
@@ -0,0 +1,12 @@
+package org.bluedevel.hollidaymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollidaymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollidaymanagerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/bluedevel/hollidaymanager/HollidaymanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.bluedevel.hollidaymanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HollidaymanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
