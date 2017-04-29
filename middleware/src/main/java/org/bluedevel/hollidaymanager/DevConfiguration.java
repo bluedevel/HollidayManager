4e7115733a59ebee563c04226eef11879a2d9fda
robin <engel.robin@gmx.de>
2017-04-29 23:10:35 +0200

    add dev profile and h2 console

---
+++ middleware/src/main/java/org/bluedevel/hollidaymanager/DevConfiguration.java
@@ -0,0 +1,23 @@
+package org.bluedevel.hollidaymanager;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @author Robin Engel
+ */
+@Configuration
+@Profile("dev")
+public class DevConfiguration {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+
+}
