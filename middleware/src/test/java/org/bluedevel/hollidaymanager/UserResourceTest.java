ec57fc2b71b2daec2aa2eb4010801bfefa00e18c
robin <engel.robin@gmx.de>
2017-04-30 21:07:31 +0200

    first running tests

--- middleware/src/test/java/org/bluedevel/hollidaymanager/UserResourceTest.java
+++ middleware/src/test/java/org/bluedevel/hollidaymanager/UserResourceTest.java
@@ -2,14 +2,23 @@ package org.bluedevel.hollidaymanager;
 
 import org.junit.Test;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  * @author Robin Engel
  */
 public class UserResourceTest extends BaseTest {
 
     @Test
-    public void test() throws InterruptedException {
-        Thread.sleep(30000);
+    public void getUser() throws Exception {
+        this.mockMvc.perform(put("/users/" + this.userHelga.getUsername()))
+                .andExpect(status().isOk());
     }
 
+    @Test
+    public void getUserNotFound() throws Exception {
+        this.mockMvc.perform(put("/users/nonexistent"))
+                .andExpect(status().isNotFound());
+    }
 }
