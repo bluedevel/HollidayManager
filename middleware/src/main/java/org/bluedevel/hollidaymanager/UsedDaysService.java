0ffe6d35110ceece161c11a0daf3773452906bc8
robin <engel.robin@gmx.de>
2017-05-20 12:48:55 +0200

    refactor used days service

--- middleware/src/main/java/org/bluedevel/hollidaymanager/UsedDaysService.java
+++ middleware/src/main/java/org/bluedevel/hollidaymanager/UsedDaysService.java
@@ -1,15 +1,15 @@
 package org.bluedevel.hollidaymanager;
 
-import org.bluedevel.hollidaymanager.daos.GlobalWorkdayDefinitionDao;
 import org.bluedevel.hollidaymanager.models.Holiday;
-import org.bluedevel.hollidaymanager.models.User;
 import org.bluedevel.hollidaymanager.models.WorkdayDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Calendar;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author Robin Engel
@@ -17,41 +17,21 @@ import java.util.Set;
 @Component
 public class UsedDaysService {
 
-    private GlobalWorkdayDefinitionDao globalWorkdayDefinitionDao;
     private WorkdayService workdayService;
-    private WorkdayDefinitionService workdayDefinitionService;
 
     @Autowired
-    public UsedDaysService(GlobalWorkdayDefinitionDao globalWorkdayDefinitionDao,
-                           WorkdayService workdayService,
-                           WorkdayDefinitionService workdayDefinitionService) {
-        this.globalWorkdayDefinitionDao = globalWorkdayDefinitionDao;
+    public UsedDaysService(WorkdayService workdayService) {
         this.workdayService = workdayService;
-        this.workdayDefinitionService = workdayDefinitionService;
     }
 
-    public double getUsedDays(User user) {
-        return getUsedDays(getEffectiveWorkdayDefinition(user), user.getHolidays());
-    }
-
-    public double getUsedDays(User user, Holiday newHoliday) {
-        Set<Holiday> holidays = new HashSet<>(user.getHolidays());
-        holidays.add(newHoliday);
-        return getUsedDays(getEffectiveWorkdayDefinition(user), holidays);
-    }
-
-    private WorkdayDefinition getEffectiveWorkdayDefinition(User user) {
-        return workdayDefinitionService.getEffectiveWorkdayDefinition(
-                globalWorkdayDefinitionDao.findAll().iterator().next(),
-                user.getUserWorkdayDefinition());
-    }
-
-    private double getUsedDays(WorkdayDefinition workdayDefinition, Collection<Holiday> holidays) {
+    public double getUsedDays(WorkdayDefinition workdayDefinition, Collection<Holiday> holidays) {
         double usedDays = 0;
         for (Holiday holiday : holidays) {
-            usedDays += holiday.getDays().stream()
+            List<Calendar> workdays = holiday.getDays().stream()
                     .filter(d -> workdayService.isWorkday(d, workdayDefinition))
-                    .count();
+                    .collect(toList());
+
+            usedDays += workdays.size();
 
             if (holiday.isStartsWithHalfDay()) {
                 usedDays -= 0.5;
--- middleware/src/main/java/org/bluedevel/hollidaymanager/resources/HolidayResource.java
+++ middleware/src/main/java/org/bluedevel/hollidaymanager/resources/HolidayResource.java
@@ -1,10 +1,13 @@
 package org.bluedevel.hollidaymanager.resources;
 
 import org.bluedevel.hollidaymanager.UsedDaysService;
+import org.bluedevel.hollidaymanager.WorkdayDefinitionService;
+import org.bluedevel.hollidaymanager.daos.GlobalWorkdayDefinitionDao;
 import org.bluedevel.hollidaymanager.daos.HolidayDao;
 import org.bluedevel.hollidaymanager.daos.UserDao;
 import org.bluedevel.hollidaymanager.models.Holiday;
 import org.bluedevel.hollidaymanager.models.User;
+import org.bluedevel.hollidaymanager.models.WorkdayDefinition;
 import org.bluedevel.hollidaymanager.resources.exceptions.NoMoreHolidayLeftException;
 import org.bluedevel.hollidaymanager.resources.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
@@ -24,13 +30,21 @@ public class HolidayResource {
 
     private UserDao userDao;
     private HolidayDao holidayDao;
+    private GlobalWorkdayDefinitionDao globalWorkdayDefinitionDao;
     private UsedDaysService usedDaysService;
+    private WorkdayDefinitionService workdayDefinitionService;
 
     @Autowired
-    public HolidayResource(UserDao userDao, HolidayDao holidayDao, UsedDaysService usedDaysService) {
+    public HolidayResource(UserDao userDao,
+                           HolidayDao holidayDao,
+                           GlobalWorkdayDefinitionDao globalWorkdayDefinitionDao,
+                           UsedDaysService usedDaysService,
+                           WorkdayDefinitionService workdayDefinitionService) {
         this.userDao = userDao;
         this.holidayDao = holidayDao;
+        this.globalWorkdayDefinitionDao = globalWorkdayDefinitionDao;
         this.usedDaysService = usedDaysService;
+        this.workdayDefinitionService = workdayDefinitionService;
     }
 
     @RequestMapping(path = "/{user}", method = PUT)
@@ -38,7 +52,14 @@ public class HolidayResource {
         User user = userDao.findByUsername(userName)
                 .orElseThrow(UserNotFoundException::new);
 
-        if (usedDaysService.getUsedDays(user, newHoliday) > user.getVacationDays()) {
+        List<Holiday> holidays = new ArrayList<>(user.getHolidays());
+        holidays.add(newHoliday);
+
+        WorkdayDefinition workdayDefinition = workdayDefinitionService.getEffectiveWorkdayDefinition(
+                globalWorkdayDefinitionDao.findAll().iterator().next(),
+                user.getUserWorkdayDefinition());
+
+        if (usedDaysService.getUsedDays(workdayDefinition, holidays) > user.getVacationDays()) {
             throw new NoMoreHolidayLeftException();
         }
 
