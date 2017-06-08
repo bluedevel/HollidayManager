b9f845abdc0e43b2ffd4cc16359f51e4204e869a
robin <engel.robin@gmx.de>
2017-06-08 08:13:50 +0200

    Vue with materialize-css (semantic still there but excluded)

---
+++ standalone-app/app/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <p>{{ message }}</p>
+
+        <a id="fancy-btn" class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                message: "Hallo Vue"
+            }
+        }
+    }
+</script>
