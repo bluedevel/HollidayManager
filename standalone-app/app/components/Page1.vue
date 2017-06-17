4821ed5bf4390bb3fad749a6f21416b3db8c1f95
robin <engel.robin@gmx.de>
2017-06-17 12:08:13 +0200

    start building custom calendar header

--- standalone-app/app/components/Page1.vue
+++ standalone-app/app/components/Page1.vue
@@ -2,6 +2,10 @@
     <div>
         <a class="waves-effect waves-light btn">Test</a>
         <p id="message">{{ message }}</p>
+
+        <div id="cal-header">
+            <span>{{ currentDate }}</span>
+        </div>
         <div id="calendar"></div>
     </div>
 </template>
@@ -9,19 +13,30 @@
 <script>
     import $ from "jquery"
 
+    let cal;
+    const data = {
+        message: "Page 1",
+        currentDate: ''
+    };
+
+    let viewRender = (view, element) => {
+        data.currentDate = cal.fullCalendar('getDate').format();
+    };
+
     export default {
-        data() {
-            return {
-                message: "Page 1"
-            }
+
+        data()
+        {
+            return data;
         },
 
-        mounted() {
-            const cal = $('#calendar');
+        mounted()
+        {
+            cal = $('#calendar');
             cal.fullCalendar({
                 editable: true,
                 displayEventTime: true,
-                header: false,
+                //header: true,
                 events: [
                     {
                         title: 'This is a Material Design event!',
@@ -29,17 +44,19 @@
                         end: new Date(),
                         color: '#C2185B'
                     }
-                ]
+                ],
+
+                viewRender
             });
 
             // tinker with the style to make the buttons materialize
             // nativ fullcalendar classes: fc-next-button fc-button fc-state-default fc-corner-right btn
             /*cal.find('button')
-                .addClass('waves-effect waves-light btn')
-                .removeClass('fc-next-button')
-                .removeClass('fc-button')
-                .removeClass('fc-state-default')
-                .removeClass('fc-corner-right');*/
+             .addClass('waves-effect waves-light btn')
+             .removeClass('fc-next-button')
+             .removeClass('fc-button')
+             .removeClass('fc-state-default')
+             .removeClass('fc-corner-right');*/
 
         }
     }
