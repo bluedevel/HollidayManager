4d6fd58e376793de23f60392b70d37c11d8badc4
robin <engel.robin@gmx.de>
2017-06-17 22:59:54 +0200

    refactoring

--- standalone-app/app/App.vue
+++ standalone-app/app/App.vue
@@ -3,7 +3,7 @@
         <nav>
             <div class="nav-wrapper">
                 <ul class="">
-                    <li><router-link to="/page1">Page 1</router-link></li>
+                    <li><router-link to="/overview">Overview</router-link></li>
                     <li><router-link to="/page2">Page 2</router-link></li>
                     <li><router-link to="/page3">Page 3</router-link></li>
                 </ul>
--- standalone-app/app/routes.js
+++ standalone-app/app/routes.js
@@ -1,9 +1,9 @@
-import Page1 from "./components/Page1.vue";
+import Overview from "./components/Overview.vue";
 import Page2 from "./components/Page2.vue";
 import Page3 from "./components/Page3.vue";
 
 export default [
-    {path: "/page1", component: Page1},
+    {path: "/overview", component: Overview},
     {path: "/page2", component: Page2},
     {path: "/page3", component: Page3}
 ]
