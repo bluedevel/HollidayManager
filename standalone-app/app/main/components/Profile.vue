e916c793fb0c120e734b8224a8e81086079ea18f
robin <engel.robin@gmx.de>
2017-06-28 19:36:40 +0200

    display complete userdata

--- standalone-app/app/main/components/Profile.vue
+++ standalone-app/app/main/components/Profile.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        {{ user.username }}
+        <ul v-if="user" class="collection with-header">
+            <li class="collection-header"><h4>{{ user.firstName }} {{user.lastName }}</h4></li>
+            <li class="collection-item"><div>Username<span class="secondary-content">{{ user.username }}</span></div></li>
+            <li class="collection-item"><div>Role<span class="secondary-content">{{ user.role }}</span></div></li>
+            <li class="collection-item"><div>Department<span class="secondary-content">{{ user.department.name }}</span></div></li>
+            <li class="collection-item"><div>Total vacation days<span class="secondary-content">{{ user.vacationDays }}</span></div></li>
+        </ul>
     </div>
 </template>
 
@@ -8,10 +14,12 @@
     import HTTP from '../../http'
 
     const data = {
-        user: {}
+        user: {
+            department: {}
+        }
     };
 
-    function mounted() {
+    function created() {
 
         HTTP.get('/users/current')
             .then((resp) => {
@@ -25,6 +33,6 @@
 
     export default {
         data: () => data,
-        mounted
+        created
     }
 </script>
