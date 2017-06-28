b6829ff552cad4c5928a024f5c7e4f7552daa8f9
robin <engel.robin@gmx.de>
2017-06-28 17:54:28 +0200

    add profile page boiler plate

--- standalone-app/app/main/Main.vue
+++ standalone-app/app/main/Main.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <nav>
             <div class="nav-wrapper">
-                <ul class="">
+                <ul class="left">
                     <li>
                         <router-link to="overview">Overview</router-link>
                     </li>
@@ -13,6 +13,9 @@
                         <router-link to="page3">Page 3</router-link>
                     </li>
                 </ul>
+                <ul class="right">
+                    <router-link to="profile"><i class="material-icons">face</i></router-link>
+                </ul>
             </div>
         </nav>
         <router-view></router-view>
@@ -22,3 +25,11 @@
 <script>
     export default {}
 </script>
+
+<style>
+
+    .right {
+        margin-right: 20px;
+    }
+
+</style>
--- standalone-app/app/routes.js
+++ standalone-app/app/routes.js
@@ -3,6 +3,7 @@ import Main from "./main/Main.vue";
 import Overview from "./main/components/Overview.vue";
 import TestLogin from "./main/components/TestLogin.vue";
 import Page3 from "./main/components/Page3.vue";
+import Profile from "./main/components/Profile.vue";
 
 export default [
     {path: "/", redirect: "/main/overview"},
@@ -12,7 +13,8 @@ export default [
         children: [
             {path: "overview", component: Overview},
             {path: "testLogin", component: TestLogin},
-            {path: "page3", component: Page3}
+            {path: "page3", component: Page3},
+            {path: "profile", component: Profile}
         ]
     },
 ]
